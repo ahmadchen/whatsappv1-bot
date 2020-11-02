6d62f8d80b59713eda9f3e88053296a7799af1a5
Mrlinkerrorsystem <54057540+ahmadchen@users.noreply.github.com>
2020-11-03 05:15:50 +0700

    @ahmadchen Upload Files
    Setup Install

---
+++ fn.sh
@@ -0,0 +1,39 @@
+sudo apt-get install -y mc
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get install -y curl
+sudo apt-get install -y zip
+sudo apt-get install -y unzip
+curl -sL https://deb.nodesource.com/setup_14.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
+sudo apt-get install -y nodejs
+sudo apt-get install -y gcc g++ make
+sudo apt-get install -y python3-pip
+sudo apt-get install -y git
+sudo apt-get install -y python3-pafy
+sudo apt-get install -y ffmpeg
+sudo apt-get install -y imagemagick
+sudo apt-get install -y gifsicle
+sudo apt-get install -y tesseract-ocr
+sudo apt-get install -y libtesseract-dev
+sudo apt-get install -y curl openssl libssl-dev
+sudo apt-get install -y libicu-dev libpango1.0-dev libcairo2-dev
+sudo apt-get update && sudo apt-get install -yq gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget
+sudo timedatectl set-timezone Asia/Jakarta
+sudo npm install pm2 -g
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt-get install -y ./google-chrome-stable_current_amd64.deb
+pip3 install ffmpeg
+pip3 install pydub
+pip3 install youtube-dl
+pip3 install img2pdf
+pip3 install pafy
+unzip nhentai.zip
+cd nhentai-master
+python3 setup.py install
+wget https://golang.org/dl/go1.15.3.linux-amd64.tar.gz && tar -C /usr/local -xzf go1.15.3.linux-amd64.tar.gz && export PATH=$PATH:/usr/local/go/bin
+cd ~
+go get github.com/Rhymen/go-whatsapp && go get github.com/Baozisoftware/qrcode-terminal-go && go get github.com/sizeofint/gif-to-webp && go get github.com/slayer/autorestart && go get && go get github.com/tidwall/gjson && go get github.com/tidwall/sjson && golang.org/x/image/bmp && golang.org/x/image/tiff && github.com/chai2010/webp
+cd fn-whatsapp-bot
+npm i
+npm start
